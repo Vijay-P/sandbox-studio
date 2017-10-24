1dfde66a64253fb376d2869d10ec59358025d71f
Vijay Pillai <vp296@cornell.edu>
2017-10-23 20:09:34 -0400

    changed appearances

---
+++ Assets/Scripts/ScaleTexture.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleTexture : MonoBehaviour {
+
+	public float scaleFactor;
+	private Renderer render;
+
+	// Use this for initialization
+	void Start () {
+		render = GetComponent<Renderer>();
+		render.material.SetTextureScale("_MainTex", new Vector2(scaleFactor, scaleFactor));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
