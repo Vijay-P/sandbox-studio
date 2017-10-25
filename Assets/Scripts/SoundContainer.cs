a5d0cee4a397ad368e7355fd71f4cb68a1e31a7e
Max Shatkhin <ms3448@cornell.edu>
2017-10-25 01:08:48 -0400

    Nearly there

---
+++ Assets/Scripts/SoundContainer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundContainer : MonoBehaviour {
+
+	public AudioClip sound;
+
+	// Use this for initialization
+	void Start () {
+		if (sound != null)
+			gameObject.GetComponentInChildren<TextMesh> ().text = sound.name;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
